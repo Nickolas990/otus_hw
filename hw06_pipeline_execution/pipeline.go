8fcf8bb09349f2e3827487aacd02ed34db86b17e
nmelnikov <nmelnikov@diasoft.ru>
2024-10-20 18:56:45 +0300

    hw06 completed

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -1,5 +1,7 @@
 package hw06pipelineexecution
 
+import "log"
+
 type (
 	In  = <-chan interface{}
 	Out = In
@@ -9,6 +11,42 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	for _, stage := range stages {
+		in = stageWrapper(in, done, stage)
+	}
+	return in
+}
+
+func stageWrapper(in In, done In, stage Stage) Out {
+	out := make(Bi)
+	go func() {
+		defer close(out)
+		stageOut := stage(in)
+		for {
+			select {
+			case v, ok := <-stageOut:
+				if !ok {
+					return
+				}
+				select {
+				case out <- v:
+				case <-done:
+					go drain(stageOut)
+					return
+				}
+			case <-done:
+				go drain(stageOut)
+				return
+			}
+		}
+	}()
+	return out
+}
+
+func drain(ch <-chan interface{}) {
+	// Draining channel to avoid goroutine leak
+	for range ch {
+		// Perform a minimal action to avoid empty block
+		log.Println("Draining channel")
+	}
 }
